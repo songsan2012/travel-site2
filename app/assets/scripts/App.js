c70d4796ff7494cc0d5e07c4811aee6ad3518307
Song <songsan2000@yahoo.com>
2017-12-01 07:30:04 -0800

    Add webpack and js files

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,11 +1,5 @@
-function Person(fullName, favColor) {
-	this.name = fullName;
-	this.favoriteColor = favColor;
-	this.greet = function() {
-		console.log("Hello, my name is " + this.name + " and my favorite color is " + this.favoriteColor + ".");
-	}
-
-}
+var $ = require('jquery');
+var Person = require('./modules/Person');
 
 var john = new Person("John Doe", "blue");
 john.greet();
@@ -13,4 +7,6 @@ john.greet();
 
 var jane = new Person("Jane Smith", "green");
 jane.greet();
+
+$("h1").remove();
 
---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+	entry: "./app/assets/scripts/App.js",
+	output: { 
+		//path: "./app/temp/scripts",
+		path: path.resolve(__dirname, './app/temp/scripts'),
+		filename: "App.js"
+		}
+} 
