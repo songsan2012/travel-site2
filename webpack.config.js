5cb436dd94e046ce5309463f63876a67cdd6a11b
Song <songsan2000@yahoo.com>
2017-12-05 07:53:38 -0800

    Add mobile menu

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,18 @@ module.exports = {
 		//path: "./app/temp/scripts",
 		path: path.resolve(__dirname, './app/temp/scripts'),
 		filename: "App.js"
+		},
+		module: {
+			loaders: [
+				{
+					loader: 'babel-loader',
+					query: {
+						presets: ['es2015']
+					},
+					test: /\.js$/,
+					exclude: /node_modules/
+				}
+			]
 		}
+		
 } 
